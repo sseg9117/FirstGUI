0bc06c17eda84cc49b540e63897cb97887d9736e
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-11-13 08:42:13 -0700

    added a button to my GUI.

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -8,16 +8,19 @@ import gui.controller.GUIAppController;
 public class GUIFrame extends JFrame
 {
 	private GUIAppController appController;
+	private GUIPanel firstPanel;
 	
 	public GUIFrame(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		this.firstPanel = new GUIPanel(appController);
 		
 		setupFrame();
 	}
 	private void setupFrame()
 	{
+		this.setContentPane(firstPanel);
 		this.setTitle("GUIFrame Window");
 		this.setSize(500,500);
 		this.setResizable(false);
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,24 +1,50 @@
 package gui.view;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
 import gui.controller.GUIAppController;
 
-import java.awt.Color.*;
-
-import javax.swing.*;
-
-public class GUIPanel
+public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
-	
 	private JButton firstButton;
-	
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
-		firstButton = new JButton("Wow a button");
-				
-				
+		firstButton = new JButton("Wow a button");	
+		baseLayout = new SpringLayout();	
+		
+		setupPanel();
+	}
+	
+	/**
+	 * Used to add all components to the subclass of JPanel
+	 * This installs them into the panel so they are seen in the GUI.
+	 */	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+	}
+	
+	/**
+	 * This is used to hold all the layout and arrangement code for SpringLayout.
+	 */
+	private void setupLayout()
+	{
+		
+	}
+	
+	/**
+	 * 
+	 */
+	private void setupListeners()
+	{
+		
 	}
 }
